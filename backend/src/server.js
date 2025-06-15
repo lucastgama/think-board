255f638777fbeff9e2e3b8d823e3d339e4b11aa7
lucastgama <lucas_tgama@hotmail.com>
2025-06-15 19:01:17 -0300

    feat: Frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import routes from "./routes/notesRoutes.js";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
@@ -9,10 +10,15 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+app.use(cors(
+  {
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }
+));
 app.use(express.json());
 app.use(rateLimiter);
-
 app.use("/api/notes", routes);
 
 connectDB().then(() => {
